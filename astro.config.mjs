29635bb8e3019dd0aebbfd85e6010167c562847d
luyongfu <594832524@qq.com>
2025-04-09 09:29:37 +0800

    first

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,8 +7,6 @@ import rehypeKatex from "rehype-katex";
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://lyg6666.github.io',
-  base: '/',
   integrations: [tailwind()],
   markdown: {
     remarkPlugins: [remarkMath],
